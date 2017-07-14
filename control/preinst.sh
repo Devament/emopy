e153a3ab0d8795880e0e49627600bc9db9c9c3d1
Wenwei Huang <dut.hww@gmail.com>
2017-07-14 16:54:11 +0800

    Add control script

---
+++ control/preinst.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [[ -e /usr/bin/hr ]]; then
+    hr cmd pip2_install dlib
+    hr cmd pip2_install tensorflow
+    hr cmd pip2_install h5py
+    hr cmd pip2_install keras>=2.0
+fi
