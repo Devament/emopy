909b2d7a9f2886f92641d19e2a3f10c1cfe2b17a
Wenwei <wenwei-dev@users.noreply.github.com>
2017-07-19 12:22:11 +0800

    Update preinst.sh

--- control/preinst.sh
+++ control/preinst.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -e /usr/bin/hr ]]; then
+    hr cmd pip2_install opencv-python
     hr cmd pip2_install dlib
     hr cmd pip2_install tensorflow
     hr cmd pip2_install h5py
